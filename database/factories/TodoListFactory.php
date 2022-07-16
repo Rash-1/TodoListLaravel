29ba138ec617e9095019b85f5f10db3b8e10dd63
mohammadhoseindolati <90058570+mohammadhoseindolati@users.noreply.github.com>
2022-07-16 10:38:51 +0430

    make : factory & seeder for todolist & category model

---
+++ database/factories/TodoListFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TodoList>
+ */
+class TodoListFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->realText(100),
+            'description' => $this->faker->realText(),
+            'category_id' => 1 ,
+        ];
+    }
+}
