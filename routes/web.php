0e44dfb12426c4d91e334842c5c479fa976d2d9b
mohammadhoseindolati <90058570+mohammadhoseindolati@users.noreply.github.com>
2022-07-19 18:02:05 +0430

    add new route

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,21 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/' , [\App\Http\Controllers\TodoListController::class , 'home']) ;
+Route::get('/todolists' , [\App\Http\Controllers\TodoListController::class , 'index']);
+Route::get('/todolists/create' , [\App\Http\Controllers\TodoListController::class , 'create']);
+Route::post('/todolists/store' , [\App\Http\Controllers\TodoListController::class , 'store']);
+Route::get('/todolists/{id}' , [\App\Http\Controllers\TodoListController::class , 'singlePage']);
+Route::get('/todolists/{id}/edit' , [\App\Http\Controllers\TodoListController::class , 'edit']);
+Route::patch('/todolists/{id}/update' , [\App\Http\Controllers\TodoListController::class , 'update']);
+Route::patch('/todolists/{id}/done' , [\App\Http\Controllers\TodoListController::class , 'done']);
+Route::delete('/todolists/{id}/delete' , [\App\Http\Controllers\TodoListController::class , 'delete']);
 
-Route::get('/', function () {
-    $faker = Faker\Factory::create();
 
+Route::get('/categories' , [\App\Http\Controllers\CategoryController::class , 'index']) ;
+Route::get('/categories/create', [\App\Http\Controllers\CategoryController::class , 'create']);
+Route::post('/categories/store' , [\App\Http\Controllers\CategoryController::class , 'store']) ;
+Route::get('/categories/{id}/edit' , [\App\Http\Controllers\CategoryController::class , 'edit']) ;
+Route::patch('/categories/{id}/update' , [\App\Http\Controllers\CategoryController::class , 'update']) ;
+Route::delete('/categories/{id}/delete' , [\App\Http\Controllers\CategoryController::class , 'delete']) ;
 
-
-    $limit = 10;
-
-
-
-    for ($i = 0; $i < $limit; $i++) {
-
-        $faker->name . ', Email Address: ' . $faker->unique()->email . ', Contact No' . $faker->phoneNumber . '<br>';
-
-    }
-});
