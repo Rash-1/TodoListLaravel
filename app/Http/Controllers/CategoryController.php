4de01ced701097f6b134d1c82b46f732d335f843
mohammadhoseindolati <90058570+mohammadhoseindolati@users.noreply.github.com>
2022-07-19 17:55:17 +0430

    make: CRUD Todolist & CRUD Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoryRequest;
+use App\Http\Requests\TodoListRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+use App\Models\TodoList;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        $categories = Category::all() ;
+        return view('category.index' , ['categories' => $categories]) ;
+    }
+    public function create()
+    {
+        return view('category.create') ;
+    }
+
+    public function store(CategoryRequest $request)
+    {
+        Category::query()->create([
+            'name' => $request->get('category_name')
+        ]);
+
+        return redirect('/categories') ;
+    }
+
+    public function edit(Category $id)
+    {
+         return view('category.edit' ,['category' => $id]) ;
+    }
+    public function update(Category $id ,UpdateCategoryRequest $request )
+    {
+        $id->update([
+            'name' => $request->get('category_name')
+        ]);
+
+        return redirect('/categories') ;
+    }
+
+    public function delete(Category $id)
+    {
+        $id->delete();
+        return redirect('/categories') ;
+    }
 }
--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -2,9 +2,73 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoListRequest;
+use App\Http\Requests\UpdateTodoListRequest;
+use App\Models\Category;
+use App\Models\TodoList;
 use Illuminate\Http\Request;
 
 class TodoListController extends Controller
 {
-    //
+    public function home()
+    {
+        return view('index') ;
+    }
+    public function index()
+    {
+        $todolists = TodoList::all() ;
+        $categories = Category::all();
+        return view('todolist.index' , ['todolists' => $todolists , 'categories' => $categories]);
+    }
+
+    public function singlePage(TodoList $id)
+    {
+        return view('todolist.single' , ['todo' => $id]);
+    }
+
+    public function done(TodoList $id)
+    {
+         $id->update([
+            'done_at' => now()
+        ]);
+        return back() ;
+    }
+    public function edit(TodoList $id)
+    {
+        return view('todolist.edit' ,['todo' => $id , 'categories' => Category::all() ]) ;
+    }
+
+    public function update(UpdateTodoListRequest $request , TodoList $id)
+    {
+        $id->update([
+            'title' => $request->get('title') ,
+            'description' => $request->get('description') ,
+            'category_id' => $request->get('category_id') ,
+        ]);
+        return redirect('/todolists') ;
+    }
+
+    public function create()
+    {
+        $categories = Category::all() ;
+        return view('todolist.create' , ['categories' => $categories]) ;
+    }
+
+    public function store(TodoListRequest $request)
+    {
+
+     TodoList::query()->create([
+         'title' => $request->get('title') ,
+         'category_id' => $request->get('category_id') ,
+         'description' => $request->get('description')
+     ]);
+
+     return back();
+    }
+
+    public function delete(TodoList $id)
+    {
+        $id->delete() ;
+        return back() ;
+    }
 }
